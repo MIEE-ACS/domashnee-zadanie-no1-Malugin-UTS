3dd8b51aa45434a63f06d5edccd20cf4b2063ef9
Malugin-UTS <121063664+Malugin-UTS@users.noreply.github.com>
2022-12-20 01:02:12 +0300

    ДЗ 1  вариант 9

---
+++ hw1_9/hw1_9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hw1_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите А");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите B");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите C");
+            int c = int.Parse(Console.ReadLine());
+            int buf1 = c;
+            int buf2 = b;
+            b = a;
+            c = buf2;
+            a = buf1;
+            Console.WriteLine($"A: {a} \n B: {b}\n C: {c}");
+        }
+    }
+}
